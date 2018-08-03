6f57d42e90591ef82d08c7e1b3b0812e7f01fe36
Ryan Mackenzie <rmackenzie@therobertstreethub.com>
2018-08-03 11:49:29 +0100

    Add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -67,8 +67,29 @@ UserSchema.statics.findByToken = function (token) {
       'tokens.token': token,
       'tokens.access': 'auth'
     });
-
 };
+
+// statics are for model methods
+UserSchema.statics.findByCredentials = function (email, password) {
+  var User = this;
+
+  return User.findOne({email}).then((user) => {
+      if (!user) {
+        return Promise.reject();
+      }
+
+      return new Promise((resolve, reject) => {
+        bcrypt.compare(password, user.password, (err, res) => {
+          if (res) {
+            resolve(user);
+          } else {
+            reject(user);
+          }
+        });
+      });
+  });
+};
+
 
 UserSchema.pre('save', function (next) {
   var user = this;
